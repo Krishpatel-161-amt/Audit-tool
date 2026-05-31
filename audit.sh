0bcfaeeb65e0f5f916fca27dd262b9586579eb92
Krish <24amtics161@gmail.com>
2026-05-31 11:01:40 +0000

    Added variables and spacing for better readability

--- audit.sh
+++ audit.sh
@@ -1,14 +1,30 @@
+report="reports/report.log"
+hname=$(hostname)
+date=$(date)
 mkdir -p reports
-echo "--- Hostname ---"> reports/report.log
-hostname >> reports/report.log 
-echo "--- report file ---" >> reports/report.log
-date >> reports/report.log
-echo "--- all user accounts ---" >> reports/report.log
-cat /etc/passwd >> reports/report.log
-echo "--- all login accounts ---" >> reports/report.log
-grep "/bin/bash" /etc/passwd >> reports/report.log
-echo "--- Sensitive Files world-writable ---" >> reports/report.log
-find ~ -perm -o+w >> reports/report.log 2> reports/error.log
+echo "Your Hostname is $hname"> $report
+
+echo"" >> $report 
+
+echo "Today is $date" >> $report
+
+echo"" >> $report
+
+echo "--- all user accounts ---" >> $report
+echo"" >> $report
+cat /etc/passwd >> $report
+
+echo"" >> $report
+
+echo "--- all login accounts ---" >> $report
+echo"" >> $report
+grep "/bin/bash" /etc/passwd >> $report
+
+echo"" >> $report
+
+echo "--- Sensitive Files world-writable ---" >> $report
+echo"" >> $report
+find ~ -perm -o+w >> $report 2> reports/error.log
 
 
 
