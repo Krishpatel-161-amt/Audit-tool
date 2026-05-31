a15427dea66fad24b37e9a25381a3ff00c270b42
Krish <24amtics161@gmail.com>
2026-05-31 11:04:04 +0000

    fixed spacing

--- audit.sh
+++ audit.sh
@@ -4,26 +4,26 @@ date=$(date)
 mkdir -p reports
 echo "Your Hostname is $hname"> $report
 
-echo"" >> $report 
+echo "" >> $report 
 
 echo "Today is $date" >> $report
 
-echo"" >> $report
+echo "" >> $report
 
 echo "--- all user accounts ---" >> $report
-echo"" >> $report
+echo "" >> $report
 cat /etc/passwd >> $report
 
-echo"" >> $report
+echo "" >> $report
 
 echo "--- all login accounts ---" >> $report
-echo"" >> $report
+echo "" >> $report
 grep "/bin/bash" /etc/passwd >> $report
 
-echo"" >> $report
+echo "" >> $report
 
 echo "--- Sensitive Files world-writable ---" >> $report
-echo"" >> $report
+echo "" >> $report
 find ~ -perm -o+w >> $report 2> reports/error.log
 
 
